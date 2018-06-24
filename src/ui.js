fcbc9e7f260f73d791e76ec395bbb09071545133
uanoob <usnet@ukr.net>
2018-06-24 16:30:23 +0300

    Add post & show alert

--- src/ui.js
+++ src/ui.js
@@ -27,6 +27,38 @@ class UI {
     });
     this.posts.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert Alert div
+    container.insertBefore(div, posts);
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
 }
 
 export const ui = new UI();
